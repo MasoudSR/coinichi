c08d1fe3dc557629ef7524a3152209ccea2c2121
Masoud S.R <amir.sepehr72@gmail.com>
2024-06-13 15:43:46 +0330

    show and hide id button added

--- src/components/SettingsPage.js
+++ src/components/SettingsPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Loading from "./Loading";
-import { IoSettingsOutline } from "react-icons/io5";
+import { IoSettingsOutline, IoCopyOutline } from "react-icons/io5";
 import { IoIosArrowDown } from "react-icons/io";
 
 function SettingsPage({ page, setPage, user, setUser }) {
@@ -8,6 +8,7 @@ function SettingsPage({ page, setPage, user, setUser }) {
 	const [isLoading, setIsLoading] = useState(false);
 	const [changeNameToggle, setChangeNameToggle] = useState(false);
 	const [error, setError] = useState("");
+	const [showId, setShowId] = useState(false);
 
 	const showError = (error) => {
 		setError(error);
@@ -50,6 +51,7 @@ function SettingsPage({ page, setPage, user, setUser }) {
 	const minimizeSettings = () => {
 		setPage("");
 		setChangeNameToggle(false);
+		setShowId(false);
 	};
 
 	return (
@@ -59,13 +61,8 @@ function SettingsPage({ page, setPage, user, setUser }) {
 				className={`bg-slate-900 p-4 rounded-t-2xl text-white max-w-xl flex flex-col gap-4  fixed bottom-20 left-0 w-full transition-all duration-500 mx-auto h-[100%] z-20 inset-x-0 ${
 					page === "settings" ? "translate-y-[40%]" : " translate-y-[100%]"
 				}`}>
-				<div className="flex justify-between mb-2">
-					<div>
-						<p className="text-lg">{user.name}</p>
-						<div className="text-[8px] text-gray-300" onClick={() => navigator.clipboard.writeText(user.id)}>
-							id: {user.id}
-						</div>
-					</div>
+				<div className="flex justify-between items-center mb-2">
+					<p className="text-3xl ml-2">{user.name}</p>
 					<button
 						className="border-white border w-11 h-11 rounded-xl flex justify-center items-center"
 						onClick={() => (page === "" ? setPage("settings") : minimizeSettings())}>
@@ -80,29 +77,55 @@ function SettingsPage({ page, setPage, user, setUser }) {
 						{error}
 					</p>
 				</div>
-				<div>
+				<div className={`transition-all duration-500 ${changeNameToggle ? "h-28" : "h-12"}`}>
 					<input
 						type="text"
 						className="text-black w-full rounded-lg p-2 mb-2"
-						placeholder="Enter You're new name"
+						placeholder="Enter You're New Name"
 						value={newName}
 						onChange={(e) => setNewName(e.target.value)}
 					/>
 					<div className={`flex transition-all relative duration-500 ${changeNameToggle || "-translate-y-[50px]"}`}>
 						<button
-							className={`bg-blue-500 p-3  transition-all duration-500 z-20 rounded-lg w-full ${
+							className={`bg-blue-500 p-3 transition-all duration-500 z-20 rounded-lg w-full ${
 								changeNameToggle ? "max-w-[67%]" : "max-w-[1000px]"
 							}`}
 							onClick={() => (changeNameToggle ? nameChangeHandler() : setChangeNameToggle(true))}>
 							{changeNameToggle ? "OK" : "Change Name"}
 						</button>
 						<button
-							className={`bg-red-400 w-[30%] block absolute right-0 top-0 z-10 rounded-lg p-3`}
+							className={`bg-red-400 w-[30%] transition-all duration-500 block absolute right-0 top-0 z-10 rounded-lg p-3 ${
+								changeNameToggle ? "scale-100" : "scale-0"
+							} `}
 							onClick={() => {
 								setChangeNameToggle(false);
 								setNewName("");
 							}}>
 							Cancel
+						</button>
+					</div>
+				</div>
+				<div
+					className={`transition-all duration-500 bg-slate-500 rounded-lg overflow-hidden ${
+						showId ? "max-h-56" : "max-h-12"
+					}`}>
+					<button
+						className={` p-3 transition-all duration-500 z-20 w-full shadow-md ${
+							showId ? "bg-slate-300 text-black" : "bg-slate-500 text-white"
+						} `}
+						onClick={() => setShowId(!showId)}>
+						{showId ? "Hide ID" : "Show ID"}
+					</button>
+					<div className="p-4 flex text-white justify-between items-center">
+						<div>
+							<p className="text-xs">Keep you&apos;re id to recover account</p>
+							<p>{user.id}</p>
+						</div>
+						<button
+							className="flex flex-col border justify-center items-center rounded-lg h-12 w-12 border-gray-200"
+							onClick={() => navigator.clipboard.writeText(user.id)}>
+							<IoCopyOutline />
+							<span className="text-[8px]">Copy</span>
 						</button>
 					</div>
 				</div>
