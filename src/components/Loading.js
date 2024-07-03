377607c3db334a12047042f6bd965cf7d803e8aa
Masoud S.R <amir.sepehr72@gmail.com>
2024-07-03 16:10:41 +0330

    loading page blur problem solved

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Loading() {
 	return (
-		<div className="fixed flex flex-col gap-2 justify-center items-center bg-gray-500/20 w-full h-full z-30 backdrop-blur-md font-bold text-white">
+		<div className="fixed top-0 flex flex-col gap-2 justify-center items-center bg-gray-500/20 w-full h-full z-30 backdrop-blur-md font-bold text-white">
 			<div className="animate-bounce text-7xl">🪙</div>
 			<p className="drop-shadow-lg">Loading</p>
 		</div>
