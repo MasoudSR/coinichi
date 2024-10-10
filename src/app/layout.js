b2ac137968c6e60eeadd1646756d9210d09f90c8
Masoud S.R <amir.sepehr72@gmail.com>
2024-10-10 09:37:15 +0330

    favicon added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,14 +4,17 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Coinichi",
-  description: "a simple clicker game",
+	title: "Coinichi",
+	description: "a simple clicker game",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
+			</head>
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
 }
